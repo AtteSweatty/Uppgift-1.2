0e4b74518c1d9ae75bf07e8a005ee35ebcee295b
arha06001 <arha06001@E-PW0234JA>
2023-09-06 10:58:16 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Atte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("404 error");
+
+        }
+    }
+}
